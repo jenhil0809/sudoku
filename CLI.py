800e5bd2ef2b4e107a40c60786d9b1f057d44e13
jenhil0809 <jenhil0809@highgateschool.org.uk>
2024-05-07 10:23:20 +0100

    Check for when puzzle is complete and comments added

--- CLI.py
+++ CLI.py
@@ -1,6 +1,7 @@
 import new
 
 
+# displays the puzzle
 def display_puzzle(text):
     for i in range(9):
         print("{:^3}{:^3}{:^3}|{:^3}{:^3}{:^3}|{:^3}{:^3}{:^3}".format(*[text.squares[(i, j)].val for j in range(9)]))
@@ -8,17 +9,25 @@ def display_puzzle(text):
             print("-" * 30)
 
 
-puzzle = new.Puzzle(" 7 583 2  592  3  34   65 7795   632  36971  68   27  914835 76 3 7 1495567429 13")
+solved = False
+# this should be loaded from a database
+# puzzle = new.Puzzle(" 7 583 2  592  3  34   65 7795   632  36971  68   27  914835 76 3 7 1495567429 13")
+puzzle = new.Puzzle("53467891267219534819834256785976142342685379171392485696153728428741963534528617 ")
 display_puzzle(puzzle)
-while True:
+while not solved:
     row = input("row")
     col = input("col")
     val = input("val")
     x = puzzle.do_move(val, row, col)
     if x == 1:
-        print("Error: this square is already set")
+        print("Error: this square is set originally")
     elif x == 2:
         print("Error: this square does not exist")
     elif x == 3:
         print("Error: value must be between 1 and 9")
-    display_puzzle(puzzle)
+    elif x == 0:
+        display_puzzle(puzzle)
+    else:
+        display_puzzle(puzzle)
+        print("Solved!")
+        solved = True # end of while loop
