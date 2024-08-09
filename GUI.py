e848e83fe0ca7d252eadb2d9a28e9b844deb85b6
jenhil0809 <jenhil0809@highgateschool.org.uk>
2024-08-09 16:23:41 +0100

    GUI works with main.py

--- GUI.py
+++ GUI.py
@@ -1,65 +1,50 @@
-import new
+import main
 import tkinter as tk
 
 
 class GameApp(tk.Tk):
     def __init__(self):
         super().__init__()
-        self.title = "Sudoku"
+        self.title("Sudoku")
         self.row = tk.IntVar()
         self.col = tk.IntVar()
         self.val = tk.IntVar()
-        self.puzzle = new.Puzzle(" 7 583 2  592  3  34   65 7795   632  36971  68   27  914835 76 3 7 1495567429 13")
-        self.game = Game(self)
-        self.game.pack()
+        self.game = main.Game()
+        self.game.load_game("user",
+                            "070583020059200300340006507795000632003697100680002700914835076030701495567429013")
+        self.sudoku_grid = Game(self)
+        self.sudoku_grid.pack()
 
 
 class Game(tk.Frame):
     def __init__(self, master: GameApp):
         super().__init__()
         self.master: GameApp = master
-        self.squares = [tk.Button(self, text=master.puzzle.squares[(i // 9, i % 9)].val, height=2, width=5, bg="light "
-                                                                                                               "grey",
+        self.squares = [tk.Button(self, text=master.game.puzzle.squares[i].val, height=2, width=5, bg="light grey",
                                   command=lambda val=i: self.button_clicked(val)) for i in range(81)]
         for i in range(81):
-            if master.puzzle.squares[(i // 9, i % 9)].val == "0":
+            if master.game.puzzle.squares[i].val == "0":
                 self.squares[i].config(text=" ")
-        self.squares[0].config(bg="lightblue1")
         self.val_input = tk.Scale(self, from_=0, to=9, orient="horizontal", variable=master.val)
-        self.row_input = tk.Scale(self, from_=1, to=9, orient="horizontal", variable=master.row,
-                                  command=self.changed_coord)
-        self.col_input = tk.Scale(self, from_=1, to=9, orient="horizontal", variable=master.col,
-                                  command=self.changed_coord)
         self.submit_button = tk.Button(self, text="Submit", command=self.change_val)
         self.error_message = tk.Label(self, text="", wraplength=100)
         self.place_widgets()
 
     def button_clicked(self, val):
-        self.squares[(self.master.row.get() - 1) * 9 + (self.master.col.get() - 1)].config(bg="light grey")
-        self.master.row.set(val // 9 + 1)
-        self.master.col.set(val % 9 + 1)
-        self.squares[(self.master.row.get() - 1) * 9 + (self.master.col.get() - 1)].config(bg="lightblue1")
+        self.squares[(self.master.row.get()) * 9 + (self.master.col.get())].config(bg="light grey")
+        self.master.row.set(val // 9)
+        self.master.col.set(val % 9)
+        self.squares[(self.master.row.get()) * 9 + (self.master.col.get())].config(bg="lightblue1")
 
-    def changed_coord(self, *args):
+    def changed_coord(self):
         for square in self.squares:
             square.config(bg="light grey")
-        self.squares[(self.master.row.get() - 1) * 9 + (self.master.col.get() - 1)].config(bg="lightblue1")
+        self.squares[(self.master.row.get()) * 9 + (self.master.col.get())].config(bg="lightblue1")
 
     def change_val(self):
-        if self.master.puzzle.do_move(str(self.master.val.get()), str(self.master.row.get() - 1),
-                                      str(self.master.col.get() - 1)) in [0, -1]:
-            if self.master.val.get() != 0:
-                self.squares[(self.master.row.get() - 1) * 9 + (self.master.col.get() - 1)] = tk.Button(self, text=str(
-                    self.master.val.get()), height=2, width=5)
-            else:
-                self.squares[(self.master.row.get() - 1) * 9 + (self.master.col.get() - 1)] = tk.Button(self, text="",
-                                                                                                        height=2,
-                                                                                                        width=5)
-            self.error_message.config(text="")
-        else:
-            self.error_message.config(
-                text=self.master.puzzle.do_move(str(self.master.val.get()), str(self.master.row.get() - 1),
-                                                str(self.master.col.get() - 1)))
+        self.master.game.puzzle.change_value(9*(self.master.row.get())+self.master.col.get(),
+                                             str(self.master.val.get()))
+        self.squares[9*(self.master.row.get())+self.master.col.get()].config(text=str(self.master.val.get()))
         self.place_widgets()
 
     def place_widgets(self):
@@ -70,11 +55,7 @@ class Game(tk.Frame):
         tk.Label(self, text="|\n" * 35).grid(row=0, column=3, rowspan=11)
         tk.Label(self, text="|\n" * 35).grid(row=0, column=7, rowspan=11)
         tk.Label(self, text="Value:").grid(row=0, column=12)
-        tk.Label(self, text="Column").grid(row=2, column=12)
-        tk.Label(self, text="Row").grid(row=1, column=12)
         self.val_input.grid(row=0, column=13)
-        self.row_input.grid(row=1, column=13)
-        self.col_input.grid(row=2, column=13)
         self.submit_button.grid(row=3, column=13)
         self.error_message.grid(row=4, column=12, columnspan=2, rowspan=3)
 
