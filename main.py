136237e1af2e7689693ed6ccf59d249ba9814d6c
jenhil0809 <jenhil0809@highgateschool.org.uk>
2024-10-27 10:26:24 +0000

    Added docstrings to main.py

--- main.py
+++ main.py
@@ -2,7 +2,23 @@ from random import choice, randint, seed
 
 
 class Square:
-    def __init__(self, val):
+    """Class for each cell in a sudoku"""
+
+    def __init__(self, val: str):
+        """
+        Parameters
+        ----------
+        val: str
+            string representing the value held by the cell
+        Attributes
+        ----------
+        self.original: bool
+            True if the cell is one of the cells given by the puzzle
+        self.original_val: str
+            the value of the cell before user inputs
+        self.guesses: list of strs
+            stores notes/guesses added by the user regarding this cell
+        """
         self.original = False
         self.original_val = "0"
         self.guesses = []
@@ -11,54 +27,116 @@ class Square:
         else:
             self.set_original(val)
 
-    # Change a cell's value
     def set_value(self, val):
+        """
+        Change the cell's value
+        Parameters
+        ----------
+        val: str
+            the value the cell should be set to
+        """
         if not self.original:
             self.val = val
 
     def add_guess(self, val):
+        """
+        Add a guess to guesses
+        Parameters
+        ----------
+        val: str
+            the value that should be added to the list
+        """
         if val not in self.guesses:
             self.guesses.append(val)
             self.guesses.sort()
 
-    # If one of the original cells, set the value and prevent any future changes
     def set_original(self, val):
+        """
+        If one of the original cells, set the value and prevent any future changes
+        Parameters
+        ----------
+        val: str
+            the value the cell should contain
+        """
         self.set_value(val)
         self.original_val = val
         self.original = True
 
     def reset(self):
+        """
+        Set the cell's value to its original value
+        """
         self.val = self.original_val
 
 
 class Group:
+    """A group is a set of cells where no cell should contain the same value"""
+
     def __init__(self, squares):
+        """
+        Parameters
+        ----------
+        squares: list of Squares
+            all the Square objects in the group
+        """
         self.squares = squares
 
     @property
     def valid(self):
+        """
+        Check if any non-blank cells in the group contain the same value
+        Returns
+        -------
+        bool
+            True if no repeated values, False otherwise
+        """
         vals = [square.val for square in self.squares if square.val != "0"]
         return len(vals) == len(set(vals))
 
 
 class Puzzle:
-    def __init__(self, puzzle, size: int=9):
+    """A sudoku puzzle"""
+
+    def __init__(self, puzzle: str, size: int = 9):
+        """
+        Parameters
+        ----------
+        puzzle: str
+            A string with the values that should be in the sudoku cells
+        size: int
+            The size of the sudoku puzzle (can be 4, 9 or 16)
+        Attributes
+        ----------
+        self.vals: list of strs
+            The values that can be placed in a cell
+        self.groups: list of Groups
+            All the groups that are in the puzzle
+        """
         self.size = size
         if self.size == 4:
             vals = [str(val + 1) for val in range(4)]
         elif self.size == 9:
             vals = [str(val + 1) for val in range(9)]
         else:
-            vals = [str(val + 1) for val in range(9)]+[char for char in "ABCDEFG"]
+            vals = [str(val + 1) for val in range(9)] + [char for char in "ABCDEFG"]
         self.vals = vals
-        self.squares = [Square(puzzle[i]) for i in range(self.size**2)]
+        self.squares = [Square(puzzle[i]) for i in range(self.size ** 2)]
         self.groups = ([Group([self.squares[i + j * self.size] for i in range(self.size)]) for j in range(self.size)] +
                        [Group([self.squares[i * self.size + j] for i in range(self.size)]) for j in range(self.size)] +
-                       [Group([self.squares[int((i // (self.size**0.5)) * self.size + i % (self.size**0.5) + (j % (self.size**0.5)) * (self.size**0.5) + (j // (self.size**0.5)) * (self.size**1.5))] for i in range(self.size)]) for
+                       [Group([self.squares[int((i // (self.size ** 0.5)) * self.size + i % (self.size ** 0.5) + (
+                               j % (self.size ** 0.5)) * (self.size ** 0.5) + (j // (self.size ** 0.5)) * (
+                                                        self.size ** 1.5))] for i in range(self.size)]) for
                         j in range(self.size)])
         self.check_valid()
 
     def return_clashes(self):
+        """
+        Loops through all the groups to check if any of them contain clashing values
+        Returns
+        -------
+        set
+            Empty set if no clashes, otherwise the indexes of any cells that contain a clash
+        """
         clash = []
         for group in self.groups:
             for i in group.squares:
@@ -68,24 +146,55 @@ class Puzzle:
                         clash.append(self.squares.index(j))
         return set(clash)
 
-    # Change a cell in the grid using its coordinates
     def change_value(self, square, val):
+        """
+        Change the value held by a cell in the grid using its coordinates
+        Parameters
+        ----------
+        square: int
+            the index of the cell to be altered
+        val: str
+            the value the cell should store
+        """
         if not self.squares[square].original:
             self.squares[square].set_value(val)
 
     def add_guess(self, square, val):
+        """
+        Add a guess for a cell in the grid using its coordinates
+        Parameters
+        ----------
+        square: int
+            the index of the cell to be altered
+        val: str
+            the value that has been guessed
+        """
         self.squares[square].add_guess(val)
 
-    # Return true if no repeated values in any group
     def check_valid(self):
+        """
+        Check if there are repeated values in any group
+        Returns
+        -------
+        bool
+            True if there are no repeated values, otherwise False
+        """
         return not (False in [group.valid for group in self.groups])
 
     def reset(self):
+        """
+        Set the values of all cells in the group back to their original values
+        """
         for cell in self.squares:
             cell.reset()
 
-    # Check if there are no, one or several solutions
     def num_solutions(self):
+        """
+        Check if there are no, one or several solutions
+        Returns
+        int
+            0 if no solutions, 1 if exactly one solution, 2 if multiple solutions
+        """
         if not self.solve():
             return 0
         solution = [cell.val for cell in self.squares][:-1]
@@ -96,7 +205,19 @@ class Puzzle:
             return 2
 
     def solve(self, i=0, excl=None):
-        # Every square filled
+        """
+        Attempt to solve the sudoku using backtracking
+        Parameters
+        ----------
+        i: the index of a cell currently not filled
+        excl: list
+            A valid solution to the sudoku that should be ignored
+        Returns
+        -------
+        Bool
+            True if a solution (other than that given by excl) is found, otherwise False
+        """
+        # every square filled
         if self.squares[i] == self.squares[-1]:
             if not [cell.val for cell in self.squares][:-1] == excl:
                 for n in self.vals:
@@ -121,6 +242,13 @@ class Puzzle:
 
     @property
     def completed(self):
+        """
+        Detect if the sudoku puzzle has been completed
+        Returns
+        -------
+        bool
+            True if completed, otherwise False
+        """
         if self.check_valid() and "0" not in [cell.val for cell in self.squares]:
             return True
         else:
@@ -129,17 +257,59 @@ class Puzzle:
 
 class Game:
     def __init__(self, size: int = 9):
+        """
+        The Game class allows a puzzle to be created and then holds the puzzle
+        Parameters
+        ----------
+        size: the size of the puzzle that is held
+        Attributes
+        ----------
+        self.puzzle: Puzzle|None
+            the puzzle that the Game is holding or None if no puzzle has been created
+        """
         self.puzzle: Puzzle | None = None
         self.size = size
-        if self.size==16:
-            self.vals=[char for char in "123456789ABCDEFGH"]
+        self.vals = self.set_vals()
+
+    def set_vals(self):
+        """
+        Return a list of the values that should be used in the sudoku based on the size of the sudoku
+        Returns
+        ----------
+        list of strs
+            A list containing all the values
+        """
+        if self.size == 16:
+            vals = [char for char in "123456789ABCDEFGH"]
         else:
-            self.vals=[str(digit+1) for digit in range(self.size)]
+            vals = [str(digit + 1) for digit in range(self.size)]
+        return vals
 
     def load_game(self, mode, arg: str, size=9):
-        self.size=size
+        """
+        Load a new sudoku puzzle
+        Parameters
+        ----------
+        mode: str
+            "user" if a user inputed puzzle should be loaded
+            "load" if a puzzle should be loaded from a file
+            "generate" if a new puzzle should be created
+        arg: str
+            if mode == "user", a string containing the values of the sudoku
+            if mode == "load", the index of the puzzle in the list
+            if mode == "generate", the number of blank cells
+        size: int
+            the size of the sudoku to be created
+        Returns
+        -------
+        bool
+            True if the puzzle was successfully loaded, otherwise False
+        """
+        if size != self.size:
+            self.size = size
+            self.vals = self.set_vals()
         if mode == "user":
-            if len(arg) == self.size**2:
+            if len(arg) == self.size ** 2:
                 self.puzzle = Puzzle(arg, self.size)
                 return self.puzzle.num_solutions() == 1
             else:
@@ -162,7 +332,18 @@ class Game:
             return False
 
     def generate_puzzle(self, blanks):
-        self.puzzle = Puzzle("0" * self.size**2, self.size)
+        """
+        Generate a new puzzle using backtracking
+        Parameters
+        ----------
+        blanks: int
+            Number of blank cells in the puzzle
+        Returns
+        -------
+        bool
+            True when the puzzle has been created
+        """
+        self.puzzle = Puzzle("0" * self.size ** 2, self.size)
         # Generate a full grid
         for cell in self.puzzle.squares:
             vals = [val for val in self.vals]
@@ -188,7 +369,14 @@ class Game:
             return False
 
     def create_blank(self):
-        n = randint(0, self.size**2-1)
+        """
+        Create a blank cell when generating a puzzle
+        Returns
+        -------
+        int, str
+            the index of the blank cell created and the value held by this cell before it was changed to a blank cell
+        """
+        n = randint(0, self.size ** 2 - 1)
         prev = self.puzzle.squares[n].val
         self.puzzle.squares[n].original = False
         self.puzzle.squares[n].original_val = "0"
